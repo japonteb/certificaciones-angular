63d571a1547f8f60429dda41b1435f48459a8814
Javier Aponte <javier.aponte@ceiba.com.co>
2022-03-01 11:17:56 -0500

    Ajustes para resolver fallo en Jenkins, se corrigen las pruebas para listar los clientes

--- src/app/feature/cliente/components/cliente/cliente.component.spec.ts
+++ src/app/feature/cliente/components/cliente/cliente.component.spec.ts
@@ -1,17 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ClienteComponent } from './cliente.component';
 
 describe('ClienteComponent', () => {
   let component: ClienteComponent;
   let fixture: ComponentFixture<ClienteComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ClienteComponent ]
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [ClienteComponent],
+        imports: [CommonModule, HttpClientModule, RouterTestingModule],
+      }).compileComponents();
     })
-    .compileComponents();
-  });
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ClienteComponent);
--- src/app/feature/cliente/shared/service/cliente.service.service.spec.ts
+++ src/app/feature/cliente/shared/service/cliente.service.service.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ClienteService } from './cliente.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
+import { HttpService } from 'src/app/core/services/http.service';
+import { Cliente } from '../model/cliente';
 
 describe('ClienteService', () => {
+  let httpMock: HttpTestingController;
   let service: ClienteService;
+  const apiEndpointClienteConsulta = `${environment.endpoint}/clientes`;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const injector = TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ClienteService, HttpService],
+    });
+    httpMock = injector.inject(HttpTestingController);
     service = TestBed.inject(ClienteService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    const clienteService: ClienteService = TestBed.inject(ClienteService);
+    expect(clienteService).toBeTruthy();
+  });
+
+  it('deberia listar clientes', () => {
+    const dummyClientes = [new Cliente(1, 'test nombre', 1)];
+    service.consultar().subscribe((clientes) => {
+      expect(clientes.length).toBe(1);
+      expect(clientes).toEqual(dummyClientes);
+    });
+    const req = httpMock.expectOne(apiEndpointClienteConsulta);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyClientes);
   });
 });
