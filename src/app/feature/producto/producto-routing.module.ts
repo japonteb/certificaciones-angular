704c33f3422714c32b414b696156d219c4a34fb0
Javier Aponte <javier.aponte@ceiba.com.co>
2022-03-10 16:57:23 -0500

    Ajustes visuales a la aplicación

--- src/app/feature/producto/producto-routing.module.ts
+++ src/app/feature/producto/producto-routing.module.ts
@@ -1,61 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CertificacionComponent } from './components/certificacion/certificacion.component';
-import { ClienteComponent } from './components/cliente/cliente.component';
-import { CrearCertificacionComponent } from './components/crear-certificacion/crear-certificacion.component';
+import { HomeComponent } from '@home/home.component';
 import { CrearExamenComponent } from './components/crear-examen/crear-examen.component';
 import { DetalleClienteComponent } from './components/detalle-cliente/detalle-cliente.component';
-import { ExamenComponent } from './components/examen/examen.component';
 import { ListarCertificacionComponent } from './components/listar-certificacion/listar-certificacion.component';
 import { ListarClienteComponent } from './components/listar-cliente/listar-cliente.component';
-import { ProductoComponent } from './components/producto/producto.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: ProductoComponent,
+    component: HomeComponent,
     children: [],
   },
 
   {
     path: 'certificaciones',
-    component: CertificacionComponent,
-    children: [
-      {
-        path: 'crear',
-        component: CrearCertificacionComponent,
-      },
-      {
-        path: 'listar',
-        component: ListarCertificacionComponent,
-      },
-    ],
+    component: ListarCertificacionComponent,
   },
 
   {
     path: 'clientes',
-    component: ClienteComponent,
-    children: [
-      {
-        path: 'listar',
-        component: ListarClienteComponent,
-      },
-      {
-        path: 'listar/detalle/:id',
-        component: DetalleClienteComponent,
-      },
-    ],
+    component: ListarClienteComponent,
+  },
+  {
+    path: 'clientes/detalle/:id',
+    component: DetalleClienteComponent,
   },
 
   {
     path: 'examenes',
-    component: ExamenComponent,
-    children: [
-      {
-        path: 'crear',
-        component: CrearExamenComponent,
-      },
-    ],
+    component: CrearExamenComponent,
   },
 ];
 
--- src/app/feature/producto/producto.module.ts
+++ src/app/feature/producto/producto.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AppMaterialModule } from '@shared/app-material/app-material.module';
 import { SharedModule } from '@shared/shared.module';
-import { CertificacionComponent } from './components/certificacion/certificacion.component';
-import { ClienteComponent } from './components/cliente/cliente.component';
 import { CrearCertificacionComponent } from './components/crear-certificacion/crear-certificacion.component';
 import { CrearExamenComponent } from './components/crear-examen/crear-examen.component';
 import { DetalleClienteComponent } from './components/detalle-cliente/detalle-cliente.component';
-import { ExamenComponent } from './components/examen/examen.component';
 import { ListarCertificacionComponent } from './components/listar-certificacion/listar-certificacion.component';
 import { ListarClienteComponent } from './components/listar-cliente/listar-cliente.component';
-import { ProductoComponent } from './components/producto/producto.component';
 import { ProductoRoutingModule } from './producto-routing.module';
 import { CertificacionService } from './shared/service/certificacion.service';
 import { ClienteService } from './shared/service/cliente.service';
@@ -17,14 +13,10 @@ import { ExamenService } from './shared/service/examen.service';
 
 @NgModule({
   declarations: [
-    ProductoComponent,
-    CertificacionComponent,
     CrearCertificacionComponent,
     ListarCertificacionComponent,
-    ClienteComponent,
     DetalleClienteComponent,
     ListarClienteComponent,
-    ExamenComponent,
     CrearExamenComponent,
   ],
   imports: [ProductoRoutingModule, SharedModule, AppMaterialModule],
